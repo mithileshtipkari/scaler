ac5e5aec0341334d3360c8627ecb10294903f227
mithileshtipkari <mithilesh.tipkari@gmail.com>
2022-11-07 00:22:13 +0530

    subsequence AG

---
+++ intermediate/dsa/src/main/java/com/scaler/intermediate/dsa/carry_forward/subsequence_AG.java
@@ -0,0 +1,25 @@
+package com.scaler.intermediate.dsa.carry_forward;
+
+import java.util.logging.Logger;
+
+public class subsequence_AG {
+    public static void main(String[] args) {
+        int ans = solve("ABCGAG");
+        System.out.println("ans -" + ans);
+    }
+
+    private static int solve(String A) {
+        char[] arr = A.toCharArray();
+        int N = A.length();
+        int gCount = 0;
+        int pairCount = 0;
+        if(N >= 1 && N <= Math.pow(10,5)){
+            for(int i = N-1 ; i >= 0; i--){
+                if(arr[i] == 'G')   gCount++;
+                if(arr[i] == 'A')   pairCount = pairCount + gCount;
+                pairCount = pairCount % 1000000007; //perform modulo in for loop itselfto avoid overflow
+            }
+        }
+        return pairCount;
+    }
+}
