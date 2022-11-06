93718966e38ad9511f521eb906306c1a8978655c
mithileshtipkari <mithilesh.tipkari@gmail.com>
2022-11-07 00:24:22 +0530

    TC & SC for subsequence AG

--- intermediate/dsa/src/main/java/com/scaler/intermediate/dsa/carry_forward/subsequence_AG.java
+++ intermediate/dsa/src/main/java/com/scaler/intermediate/dsa/carry_forward/subsequence_AG.java
@@ -14,7 +14,7 @@ public class subsequence_AG {
         int gCount = 0;
         int pairCount = 0;
         if(N >= 1 && N <= Math.pow(10,5)){
-            for(int i = N-1 ; i >= 0; i--){
+            for(int i = N-1 ; i >= 0; i--){//loop runs N times - N
                 if(arr[i] == 'G')   gCount++;
                 if(arr[i] == 'A')   pairCount = pairCount + gCount;
                 pairCount = pairCount % 1000000007; //perform modulo in for loop itselfto avoid overflow
@@ -23,3 +23,5 @@ public class subsequence_AG {
         return pairCount;
     }
 }
+//TC - O(N) - only one loop running N times
+//SC - O(1) - const no of variable used
