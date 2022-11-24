16d6ddb829f7f533a43e83bbd0067c17a4f49b15
mithileshtipkari <mithilesh.tipkari@gmail.com>
2022-11-24 22:08:14 +0530

    rename

--- intermediate/dsa/src/main/java/com/scaler/intermediate/dsa/ModularArithmetics/PowerWithModulus.java
+++ intermediate/dsa/src/main/java/com/scaler/intermediate/dsa/ModularArithmetics/PowerWithModulus.java
@@ -1,4 +1,4 @@
-public class PowerWithModulas{
+public class PowerWithModulus {
     public int solve(int A, int B, int C) {
         if(A>=1 && A<=1000000000 && B>=0 && B<=100000 && C>=1 && C<=1000000000){
             long ans = 1;
