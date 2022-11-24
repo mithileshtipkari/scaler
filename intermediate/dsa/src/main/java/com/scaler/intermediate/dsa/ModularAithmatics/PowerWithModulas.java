c2854b06cef8c770074f2b3603aa1a0d7ce78170
mithileshtipkari <mithilesh.tipkari@gmail.com>
2022-11-24 21:17:52 +0530

    Power with modulas

---
+++ intermediate/dsa/src/main/java/com/scaler/intermediate/dsa/ModularAithmatics/PowerWithModulas.java
@@ -0,0 +1,10 @@
+public int solve(int A, int B, int C) {
+        if(A>=1 && A<=1000000000 && B>=0 && B<=100000 && C>=1 && C<=1000000000){
+        long ans = 1;
+        for(int i=1; i<=B; i++){
+        ans = (ans * A)%C;
+        }
+        return (int)ans;
+        }
+        return -1;
+        }
