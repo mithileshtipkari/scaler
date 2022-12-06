3892b094a223984db057228199fc475f659327b4
mithileshtipkari <mithilesh.tipkari@gmail.com>
2022-12-07 01:33:52 +0530

    Count of distinct ele using hashset

---
+++ intermediate/dsa/src/main/java/com/scaler/intermediate/dsa/hashing/CountOfDistinctEle.java
@@ -0,0 +1,17 @@
+package com.scaler.intermediate.dsa.hashing;
+
+public class CountOfDistinctEle {
+    public int solve(int[] A) {
+        int n = A.length;
+        HashSet<Integer> hs = new HashSet<>();
+        if(n>=1 && n<=100000){
+            for(int z=0; z<n; z++){
+                if(A[z]>=1 && A[z]<=1000000000){
+                    hs.add(A[z]);
+                }
+            }
+            return hs.size();
+        }
+        return -1;
+    }
+}
