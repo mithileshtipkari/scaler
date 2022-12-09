7ba46e1cd8e4da9b9095322edf84906a5249415d
mithileshtipkari <mithilesh.tipkari@gmail.com>
2022-12-10 01:30:13 +0530

    Gym class example

--- intermediate/dsa/src/main/java/com/scaler/intermediate/dsa/ClassesAndObjects/Gym.java
+++ intermediate/dsa/src/main/java/com/scaler/intermediate/dsa/ClassesAndObjects/Gym.java
@@ -1,2 +1,14 @@
-package com.scaler.intermediate.dsa.ClassesAndObjects;public class Gym {
+package com.scaler.intermediate.dsa.ClassesAndObjects;
+
+public class Gym {
+    String name;
+    int noOfMembers;
+
+    public static void main(String[] args) {
+        Gym g;
+        //System.out.println(g.name);//might not have been initialized
+
+        Gym beta = null;
+        System.out.println("name -" + beta.name);//NPE
+    }
 }
